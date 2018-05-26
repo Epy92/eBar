400e51d2f69ec6f6f017be0f63107b6078dba401
IoanOnofras <ioan.onofras@gmail.com>
2018-05-26 19:43:17 +0300

    Creare Map Pentru Modele

--- eBarService/eBarWS/App_Start/AutoMapperConfig.cs
+++ eBarService/eBarWS/App_Start/AutoMapperConfig.cs
@@ -10,12 +10,22 @@ namespace eBarWS.App_Start
         {
             Mapper.Initialize((config) =>
             {
-                config.CreateMap<Restaurants, RestaurantModel>().ReverseMap();
-                config.CreateMap<UserTbl, UserModel>().ReverseMap();
                 config.CreateMap<Languages, LanguagesModel>().ReverseMap();
                 config.CreateMap<RestaurantAdministrators, RestaurantAdministratorsModel>().ReverseMap();
                 config.CreateMap<RestaurantDetails, RestaurantDetailsModel>().ReverseMap();
+                config.CreateMap<RestaurantLocations, RestaurantLocationsModel>().ReverseMap();
                 config.CreateMap<RestaurantProducts, RestaurantProductsModel>().ReverseMap();
+                config.CreateMap<RestaurantProductsCategories, RestaurantProductsCategoriesModel>().ReverseMap();
+                config.CreateMap<Restaurants, RestaurantModel>().ReverseMap();
+                config.CreateMap<RestaurantTables, RestaurantTablesModel>().ReverseMap();
+                config.CreateMap<RestaurantTypes, RestaurantTypesModel>().ReverseMap();
+                config.CreateMap<UserResetPasswordCodes, UserResetPasswordCodesModel>();
+                config.CreateMap<UserTbl, UserModel>().ReverseMap();
+                config.CreateMap<UserTypes, UserTypesModel>().ReverseMap();
+               
+                
+                config.CreateMap<RestaurantProducts, RestaurantProductsModel>().ReverseMap();
+               
             });
         }
     }
