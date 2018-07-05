a6b79720ea1bd1b20e5b718d1cbcfacc9676d498
Epy92 <catalinepingeac@gmail.com>
2018-07-05 17:54:36 +0300

    Update restaurant-events.ts

--- eBarIonic/src/pages/restaurant-pages/restaurant-events/restaurant-events.ts
+++ eBarIonic/src/pages/restaurant-pages/restaurant-events/restaurant-events.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+
+class RestaurantEventsModel{
+  EventId:number;
+  EventTitle:string;
+  EventDescription:string;
+  RestaurantThumbnail: string;
+  HasThumbnail:boolean;
+  StartDate:datetime;
+  EndDate:datetime;
+  StartDay:string;
+  StartMonth:string;
+  StartYear:string;
+  StartTime:string;
+  EndDay:string;
+  EndMonth:string;
+  EndYear:string;
+  EndTime:string;
+}
+
 @Component({
   selector: 'page-restaurant-events',
   templateUrl: 'restaurant-events.html',
 })
-export class RestaurantEventsPage {
 
+export class RestaurantEventsPage {
+events: List<RestaurantEventsModel>;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
