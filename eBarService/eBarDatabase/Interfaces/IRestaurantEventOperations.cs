fa4fe1651b25eaa8341e4491abbf4a15cd638797
IoanOnofras <ioan.onofras@gmail.com>
2018-07-21 12:52:51 +0300

    com

---
+++ eBarService/eBarDatabase/Interfaces/IRestaurantEventOperations.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using ViewModels;
+
+namespace eBarDatabase.Interfaces
+{
+    public interface IRestaurantEventOperations
+    {
+        List<RestaurantEvent> GetRestaurantEvents(int restaurantId);
+        string SaveRestaurantEvent(RestaurantEvent restaurantEvent);
+        string UpdateRestaurantEvent(RestaurantEvent restaurantEvent);
+    }
+}
