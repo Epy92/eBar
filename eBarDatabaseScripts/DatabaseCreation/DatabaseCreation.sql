4879460d0dd871d2984aea0e016b6d44ffad46cf
Epy92 <catalinepingeac@gmail.com>
2019-10-22 16:02:17 +0300

    Update DatabaseCreation.sql

--- eBarDatabaseScripts/DatabaseCreation/DatabaseCreation.sql
+++ eBarDatabaseScripts/DatabaseCreation/DatabaseCreation.sql
@@ -7,14 +7,18 @@ create table UserTypes
 	TypeName nvarchar(100) not null,
 );
 
-create table UserTbl
+create table UserDetails
 (
 	UserID int not null primary key identity(1,1),
 	Username nvarchar(100) not null,
-	UserPassword nvarchar(100) not null,
 	Email nvarchar(200) not null,
-	Name nvarchar(500) not null,
-	UserPreferredLanguage nvarchar(10) null,
+	FirstName nvarchar(300) not null,
+	LastName nvarchar(300) not null,
+	BirthDate datetime null,
+	FacebookToken nvarchar(max) null,
+	FacebookPhotoUrl nvarchar(1000) null,
+	GoogleToken nvarchar(max) null,
+	GooglePhotoUrl nvarchar(1000) null,
 	UserTypeId int not null foreign key references UserTypes(UserTypeId)
 );
 
@@ -22,32 +26,22 @@ create table Restaurants
 (
 	RestaurantId int not null primary key identity(1,1),
 	RestaurantName nvarchar(255) not null,
+	RestaurantOwner int not null foreign key references UserTypes(UserTypeId)
 );
 
-create table RestaurantLocations
+create table RestaurantDetails
 (
-	LocationId int not null primary key identity(1,1),
-	RestaurantCounty nvarchar(200) not null,
+	RestaurantDetailsId int not null primary key identity(1,1),
+	RestaurantId int not null foreign key references Restaurants(RestaurantId) on delete cascade,
+	RestaurantDirectoryGuid nvarchar(100) null,
+	RestaurantThumbnail nvarchar(max),
+	RestaurantDescription nvarchar(3000) null,
+	RestaurantPhone nvarchar(1000),
 	RestaurantCity nvarchar(200) not null,
 	RestaurantAddress nvarchar(300) null,
 	Latitude decimal(10, 6) not null,
 	Longitude decimal(10,6) not null,
-	RestaurantId int not null foreign key references Restaurants(RestaurantId)
-);
-
-create table RestaurantAdministrators
-(
-	RestaurantAdminId int not null primary key identity(1,1),
-	RestaurantId int not null foreign key references Restaurants(RestaurantId) on delete cascade,
-	UserID int not null foreign key references UserTbl(UserID),
-);
-
-create table RestaurantTables
-(
-	TableId int not null primary key identity(1,1),
-	TableNumber int not null,
-	TableBarcode nvarchar(100) not null,
-	RestaurantId int not null foreign key references Restaurants(RestaurantId) on delete cascade,
+	RestaurantProgram nvarchar(1000),
 );
 
 create table RestaurantTypes
@@ -57,31 +51,17 @@ create table RestaurantTypes
 	TypeName nvarchar(250) not null,
 );
 
-create table RestaurantProductsCategories
+create table RestaurantEvent
 (
-	CategoryId int not null primary key identity(1,1),
+	RestaurantEventID int not null primary key identity(1,1),
+	EventName nvarchar (500)  not null,
+	EventDescription nvarchar (2000) not null,
+	EventOffer  nvarchar (500)  null,
+	EventPhotoUrl nvarchar(1000) null,
+	EventStartDate datetime not null,
+	EventEndDate datetime not null,
+	EventPublicationDate  datetime not null,
 	RestaurantId int not null foreign key references Restaurants(RestaurantId) on delete cascade,
-	CategoryName nvarchar(250) not null,
-);
-
-create table RestaurantProducts
-(
-	ProductId int not null primary key identity(1,1),
-	CategoryId int not null foreign key references RestaurantProductsCategories(CategoryId),
-	ProductName nvarchar(250) not null,
-	ProductPrice decimal(10,6) not null,
-	ProductMeasurement nvarchar(100) not null,
-	ProductMeasurementValue int,
-	ProductMadeOf nvarchar(2000),
-	RestaurantId int not null foreign key references Restaurants(RestaurantId) on delete cascade,
-);
-
-create table UserResetPasswordCodes
-(
-	UserResetPwdId int not null primary key identity(1,1),
-	UserID int not null foreign key references UserTbl(UserID),
-	ResetCode nvarchar(10) not null,
-	CreationDate datetime not null,
 );
 
 create table Languages
@@ -92,13 +72,9 @@ create table Languages
 	LanguageName nvarchar(100) null
 );
 
-create table RestaurantDetails
+create table RestaurantFavorites
 (
-	RestaurantDetailsId int not null primary key identity(1,1),
+	RestaurantFavoriteID int not null primary key identity(1,1),
 	RestaurantId int not null foreign key references Restaurants(RestaurantId) on delete cascade,
-	RestaurantDirectoryGuid nvarchar(100) null,
-	RestaurantThumbnail nvarchar(max),
-	RestaurantDescription nvarchar(3000) null,
-	RestaurantContact nvarchar(1000),
-	RestaurantProgram nvarchar(1000),
+	UserID int not null foreign key references UserTbl(UserID),
 );
